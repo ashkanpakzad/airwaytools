b8d4a8f65daa90a604da0665219813056a4a6e31
apakz <ashpakzad@gmail.com>
2019-07-11 09:27:46 +0100

    Finds distal terminal points and saves to nifti file

--- DistalMarker.m
+++ DistalMarker.m
@@ -1,13 +1,15 @@
 % By Ashkan Pakzad 4th July 2019 (ashkan.pakzad.13@ucl.ac.uk)
 
+% Script to mark the distal terminals of airways.
+
 %% Params
 imagename='N1.nii.gz';
 segname='N1_lumen.nii.gz';
 
 %% Load data
-V = double(niftiread(image_name));
-I = niftiinfo(image_name);
-S = logical(niftiread(segmenation_name));
+V = double(niftiread(imagename));
+I = niftiinfo(imagename);
+S = logical(niftiread(segname));
 
 %% Skeletonise lumen segmentation & graph it
 skel = bwskel(S);
@@ -20,10 +22,26 @@ G = graph(A);
 C_Gidx = neighbors(G,length(A)); % carina node
 
 
+%% identify terminal points
+terminal_idx = find([node.ep])';
+
+% remove trachea
+terminal_idx = terminal_idx(1:end-1);
+
+%% Convert to voxel list and create logical array
+terminal_vox = [node(terminal_idx).idx];
+
+distal = zeros(size(S));
+distal(terminal_vox) = 1;
+
+%% Save distal logical array
+niftiwrite(double(distal), 'N1_distal')
+
 %% tools to Graph plot
-% figure
-% G = graph(A);
-% h = plot(G);
-% highlight(h, 115);
+
+figure
+G = graph(A);
+h = plot(G);
+highlight(h, C_Gidx); % highlight carina
 
 
